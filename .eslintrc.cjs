b6d008bc82f26627e2ad0604c1e08052c136cd8a
ruaaave <anna.kurenkova@raiffeisen.ru>
2024-03-31 22:15:05 +0300

    add eslint and next ui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,47 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": "warn",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "newline-before-return": 1,
+    "simple-import-sort/imports": [
+      "warn", // 'warn' helps sort imports
+      {
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          [
+            "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)",
+          ],
+          // `react & redux` related packages come first.
+          ["^react", "^redux", "^@reduxjs", "^@testing-library", "^@storybook"],
+          // Other packages
+          ["^@fcc", "^@mf", "^\\w"],
+          // Root path for project
+          ["^@/", "^#"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.styles?$"],
+        ],
+      },
     ],
   },
-}
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+};
